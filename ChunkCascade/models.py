67ede47f9a1c81e7be8b8ef1ab3be16122be407b
Austin Smith <atsmith522@gmail.com>
2020-03-10 17:51:50 -0400

    changes to transcend the need for low IQ macbook

--- ChunkCascade/models.py
+++ ChunkCascade/models.py
@@ -5,9 +5,11 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=256)
+    answered = models.BooleanField(default=False)
 
 class Answer(models.Model):
     answer_text = models.TextField()
+    helpful_answer = models.BooleanField(default=False)
 
 class Comment(models.Model):
     comment_text = models.TextField()
