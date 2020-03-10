f2b007db733e8b06ce2c7721b8f6b6c27f5c7bba
Austin Smith <atsmith522@gmail.com>
2020-03-10 17:55:49 -0400

    changes in models that make me remember that Clinton can see commit msgs

--- ChunkCascade/models.py
+++ ChunkCascade/models.py
@@ -1,15 +1,17 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=256)
     answered = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class Answer(models.Model):
     answer_text = models.TextField()
     helpful_answer = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class Comment(models.Model):
     comment_text = models.TextField()
